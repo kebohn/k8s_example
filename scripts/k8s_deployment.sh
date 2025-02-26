1ec2b4ef61183e997a1bce730bef6e03c42a92ac
Kevin Bohn <45978990+kebohn@users.noreply.github.com>
2025-02-26 23:22:15 +0100

    Update k8s_deployment.sh

--- scripts/k8s_deployment.sh
+++ scripts/k8s_deployment.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
+
 kubectl apply -f k8s/nginx-configmap.yaml
 kubectl apply -f k8s/nginx-deployment.yaml
 kubectl apply -f k8s/nginx-service.yaml
-kubectl apply -f k8s/backend-deployment.yaml
-kubectl apply -f k8s/backend-service.yaml
